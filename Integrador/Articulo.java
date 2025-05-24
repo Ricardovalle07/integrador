85e82b02e42a4a20292ea58d3ac072f40236e886
EdwinCardeGar <ed3997@gmail.com>
2025-05-24 16:30:15 -0600

    Update Articulo.java

--- Integrador/Articulo.java
+++ Integrador/Articulo.java
@@ -1,3 +1,39 @@
 public class Articulo {
-    
+    private int codigo, existencia; String desc; double precio;
+    public Articulo(int cod, int exis, String des, double pri){
+        codigo=cod;
+        existencia=exis;
+        desc=des;
+        precio=pri;
+    }
+    public void setCodigo(int cod){
+        codigo=cod;
+    }
+    public void setExistencia(int exis){
+        existencia=exis;
+    }
+    public void setDesc(String des){
+        desc=des;
+    }
+    public void setPrecio(double pri){
+        precio=pri;
+    }
+    public int getCodigo(){
+        return codigo;
+    }
+    public int getExistencia(){
+        return existencia;
+    }
+    public String getDesc(){
+        return desc;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    public double valorInventario(){
+        return precio*existencia;
+    }
+    public String toString(){
+        return "Codigo: "+codigo+" Descripcion: "+desc+" Precio: $"+precio+" Existencia: "+existencia;
+    }
 }
