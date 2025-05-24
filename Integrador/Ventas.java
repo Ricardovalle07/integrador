31b63efe97e4be4563744cfcdf0032a2b60e0f28
EdwinCardeGar <ed3997@gmail.com>
2025-05-24 17:24:09 -0600

    Update Ventas.java

--- Integrador/Ventas.java
+++ Integrador/Ventas.java
@@ -1,3 +1,33 @@
-public class Ventas {
-    
+public class Ventas extends Cliente{
+    private int cantidad;
+    private Articulo articulo; 
+    private Cliente cliente;
+    public Ventas(int cant, int num, String nom, Articulo art, Cliente cli){
+        super(num, nom);
+        cantidad = cant;
+        articulo = art;
+        cliente = cli;
+    }
+    public void setCantidad(int cant){
+        cantidad=cant;
+    }
+    public void setArticulo(Articulo art){
+        articulo=art;
+    }
+    public void setCliente(Cliente cli){
+        cliente=cli;
+    }
+    public int getCantidad(){
+        return cantidad;
+    }
+    public Articulo getArticulo(){
+        return articulo;
+    }
+    public Cliente getCliente(){
+        return cliente;
+    }
+    public String toString(){
+        return "Cliente: "+cliente+" Articulo: "+articulo+" Cantidad: "+cantidad;
+    }
+
 }
