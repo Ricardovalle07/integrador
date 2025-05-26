32182c2d14b3c71529ffbcef7dcefb1d6a31b184
EdwinCardeGar <ed3997@gmail.com>
2025-05-26 09:28:51 -0600

    Update CatalogoDeArticulos.java

--- Integrador/CatalogoDeArticulos.java
+++ Integrador/CatalogoDeArticulos.java
@@ -1,3 +1,12 @@
+import java.io.*;
+import javax.swing.*;
+
 public class CatalogoDeArticulos {
-    
+    public static void main(String[]args) throws IOException, FileNotFoundException{
+        File fileL = new File("");
+        FileReader leer= new FileReader(fileL);
+        BufferedReader entrada = new BufferedReader(leer);
+
+//ARCHIVOS LO INGRESA EL USUARIO 
+    }
 }
