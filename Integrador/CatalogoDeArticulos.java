0c0cbcbdf181edaca7b3cc4b0c732e50bd1d3efd
EdwinCardeGar <ed3997@gmail.com>
2025-05-26 21:43:13 -0600

    Update CatalogoDeArticulos.java

--- Integrador/CatalogoDeArticulos.java
+++ Integrador/CatalogoDeArticulos.java
@@ -3,10 +3,46 @@ import javax.swing.*;
 
 public class CatalogoDeArticulos {
     public static void main(String[]args) throws IOException, FileNotFoundException{
-        File fileL = new File("");
-        FileReader leer= new FileReader(fileL);
-        BufferedReader entrada = new BufferedReader(leer);
+        File filetxt = new File("articulos.txt");
+        FileOutputStream ft= new FileOutputStream(filetxt);
+        PrintWriter salidaT = new PrintWriter(ft);
 
-//ARCHIVOS LO INGRESA EL USUARIO 
+        File fileD = new File("articulos.dat");
+        FileOutputStream fd= new FileOutputStream(fileD);
+        ObjectOutputStream salidaD= new ObjectOutputStream(fd);
+
+        Articulo codigo;
+        int code, num, existencia;
+        String nom, desc;
+        double precio;
+
+
+        code=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el codigo del articulo:"));     
+        codigo=new Articulo(code, 0, "", 0.0, 0, "");
+        while(code>0){
+            desc=JOptionPane.showInputDialog("Ingrese la descripcion del articulo:");
+            existencia=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la existencia del articulo:"));
+            precio=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio del articulo:"));
+            num=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del cliente:"));
+            nom=JOptionPane.showInputDialog("Ingrese el nombre del cliente:");
+
+            codigo.setCodigo(code);
+            codigo.setDesc(desc);
+            codigo.setExistencia(existencia);
+            codigo.setPrecio(precio);
+            Articulo articulo = new Articulo(code, existencia, desc, precio, num, nom);
+            
+            salidaT.println(articulo.toString());
+            salidaT.println("--------------------------------");
+
+            salidaD.writeObject(articulo.toString());
+
+
+            code=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el codigo del articulo:"));
+        }
+        salidaT.close();
+        salidaD.close();
+        System.out.println("Articulos guardados en articulos.txt y articulos.dat");
+        
     }
 }
